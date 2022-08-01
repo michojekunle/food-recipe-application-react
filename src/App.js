a4865452d4d83a1c8ea8f88bd79d38428a7b967c
AMD <michojekunle1@gmail.com>
2022-08-01 15:07:10 +0100

    okay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import './App.css'
 
 const App = () => {
+  const API_ID = "11936026";
+  const API_KEY = "d07d03f53faf66beafc06604b25f4980";
+  const API_URL = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${API_ID}&app_key=${API_KEY}`;
+
+  
   return (
     <div className='App'>
       <h1>Food seacrhing app</h1>
